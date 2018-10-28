dba409cb4b72fcba8e2adaacec7f267dbf734ee0
anguyen32 <anhnd32@fsoft.com.vn>
2018-10-28 15:22:44 -0500

    Chapter 3
    Stack and queue tested. Seem ez as hell

---
+++ Chapter3/3.1/sol.py
@@ -0,0 +1,49 @@
+import sys
+import stack
+
+class Output:
+	def __init__(self):
+		self.o = sys.stdout
+	def write( self, aline ):
+		self.o.write(str(aline))
+	def writeln(self, aline):
+		self.o.write(str(aline) + '\n')
+
+class Input:
+	def __init__( self ):
+		self.i = sys.stdin
+	def readline( self ):
+		return self.i.readline()
+	def readInt( self ):
+		return int ( self.i.readline())
+	def readFlt( self ):
+		return float(self.i.readline())
+	def readToList(self,splitter = " "):
+		return list(self.i.readline().split(splitter))
+	def readToIntList(self,splitter = " "):
+		return list(map(int, self.i.readline().split(splitter)))
+	def readToFltList(self,splitter = " "):
+		return list(map(float, self.i.readline().split(splitter)))
+
+class Error:
+	def __init__(self):
+		self.e = sys.stderr
+	def write( self, aline ):
+		self.e.write(str(aline))
+	def writeln(self, aline):
+		self.e.write(str(aline) + '\n')
+
+def solve(n):
+	'''
+	write your code here
+	'''
+	return n
+
+if __name__ == '__main__':
+	O = Output()
+	E = Error()
+	I = Input()
+
+	query = I.readInt()
+	for i in range(query):
+		O.writeln(solve(I.readToFltList()))
