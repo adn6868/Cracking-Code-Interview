ab9c92a1f20124d2e2b6d7cda64bb0fac691e175
anguyen32 <anhnd32@fsoft.com.vn>
2018-10-28 13:19:42 -0500

    New IO
    IO 2 Work like a charm. would speed up a lot of stuff

--- IO/IO.py
+++ IO/IO.py
@@ -1,7 +1,4 @@
 import sys
-from sys import stdin 
-from sys import stdout 
-from sys import stderr 
 
 class Output:
 	def __init__(self):
@@ -12,7 +9,7 @@ class Output:
 		self.o.write(str(aline) + '\n')
 
 class Input:
-	def __init__(self):
+	def __init__( self ):
 		self.i = sys.stdin
 	def readline( self ):
 		return self.i.readline()
@@ -35,17 +32,17 @@ class Error:
 	def writeln(self, aline):
 		self.e.write(str(aline) + '\n')
 
-
 def solve(n):
+	'''
+	write your code here
+	'''
 	return n
-	# E.write(n)
-
 
 if __name__ == '__main__':
 	O = Output()
 	E = Error()
 	I = Input()
 
-	q = I.readInt()
-	for i in range(q):
+	query = I.readInt()
+	for i in range(query):
 		O.writeln(solve(I.readToFltList()))
