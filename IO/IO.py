0d46c6e1d483b4ae82e892e74c7047c0c579a667
anguyen32 <anhnd32@fsoft.com.vn>
2018-10-28 13:04:05 -0500

    IO 2.1 work greate
    Create a wrapper arround.
    allow read and write ez and less messy

--- IO/IO.py
+++ IO/IO.py
@@ -1,6 +1,39 @@
-from sys import stdin as I
-from sys import stdout as O
-from sys import stderr as E
+import sys
+from sys import stdin 
+from sys import stdout 
+from sys import stderr 
+
+class Output:
+	def __init__(self):
+		self.o = sys.stdout
+	def write( self, aline ):
+		self.o.write(str(aline))
+	def writeln(self, aline):
+		self.o.write(str(aline) + '\n')
+
+class Input:
+	def __init__(self):
+		self.i = sys.stdin
+	def readline( self ):
+		return self.i.readline()
+	def readInt( self ):
+		return int ( self.i.readline())
+	def readFlt( self ):
+		return float(self.i.readline())
+	def readToList(self,splitter = " "):
+		return list(self.i.readline().split(splitter))
+	def readToIntList(self,splitter = " "):
+		return list(map(int, self.i.readline().split(splitter)))
+	def readToFltList(self,splitter = " "):
+		return list(map(float, self.i.readline().split(splitter)))
+
+class Error:
+	def __init__(self):
+		self.e = sys.stderr
+	def write( self, aline ):
+		self.e.write(str(aline))
+	def writeln(self, aline):
+		self.e.write(str(aline) + '\n')
 
 
 def solve(n):
@@ -9,6 +42,10 @@ def solve(n):
 
 
 if __name__ == '__main__':
-	q = I.readline()
-	for i in range(int(q)):
-		O.write(solve(I.readline().strip('\n'))
+	O = Output()
+	E = Error()
+	I = Input()
+
+	q = I.readInt()
+	for i in range(q):
+		O.writeln(solve(I.readToFltList()))
