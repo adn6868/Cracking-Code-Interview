859e896734c697366e945041a35d53028329f559
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-13 13:00:28 -0600

    Create IO1.2.py

---
+++ IO/IO1.2.py
@@ -0,0 +1,57 @@
+import sys
+
+@warapper
+class Output:
+	def __init__(self):
+		self.o = sys.stdout
+	def write( self, aline ):
+		self.o.write(str(aline))
+	def writeln(self, aline):
+		self.o.write(str(aline) + '\n')
+
+class Input:
+	def __init__( self ):
+		self.i = sys.stdin
+	def readline( self ):
+		return self.i.readline().strip('\n')
+	def readInt( self ):
+		return int ( self.i.readline().strip('\n'))
+	def readFlt( self ):
+		return float(self.i.readline().strip('\n'))
+	def readToList(self,splitter = " "):
+		return list(self.i.readline().strip('\n').split(splitter))
+	def readToIntList(self,splitter = " "):
+		return list(map(int, self.i.readline().strip('\n').split(splitter)))
+	def readToFltList(self,splitter = " "):
+		return list(map(float, self.i.readline().strip('\n').split(splitter)))
+
+class Error:
+	def __init__(self):
+		self.e = sys.stderr
+	def write( self, aline ):
+		self.e.write(str(aline))
+	def writeln(self, aline):
+		self.e.write(str(aline) + '\n')
+
+class Solver:
+	def __init__(self):
+		self.O = Output()
+		self.E = Error()
+		self.I = Input()
+	def solve(self,n):
+		return n		
+
+def solve(n):
+	'''
+	write your code here
+	'''
+	return n
+
+if __name__ == '__main__':
+	O = Output()
+	E = Error()
+	I = Input()
+
+	query = I.readInt()
+	for i in range(query):
+		O.writeln(solve(I.readToFltList()))
